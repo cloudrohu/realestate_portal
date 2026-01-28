825f1f4488660dd736c2d92c5e36edf78cd4845e
cloudrohu <cloudrohu@gmail.com>
2026-01-28 20:32:25 +0530

    bank offers eror

--- projects/views.py
+++ projects/views.py
@@ -314,7 +314,7 @@ def project_details(request, id, slug):
     project_amenities = project.project_amenities.all().distinct()
     has_balcony = project.configurations.filter(balcony=True).exists()
     project_faqs = project.faqs.all()
-    bank = project.bankingoffers.all()
+    bank_offers = project.bank_offers.all()
 
     context = {
         "settings_obj": settings_obj,
@@ -324,7 +324,7 @@ def project_details(request, id, slug):
         "min_price": price_range["min_price"],
         "max_price": price_range["max_price"],
         "related_projects": related_projects,
-        "bank": bank,
+        "bank_offers": bank_offers,
         "project_amenities": project_amenities,
         "project_faqs": project_faqs,   # ✅ ADD THIS
         "has_balcony": has_balcony,   
